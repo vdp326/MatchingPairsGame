541ca5f924bc5239f8592bfc685325fad9d32ff8
Vu Pham <vupham326@gmail.com>
2019-01-23 01:00:34 -0800

    commit project

---
+++ MatchingPairsGame/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingPairsGame
+{
+     public partial class Form1 : Form
+     {
+          Random random = new Random(); //generate random icons
+          List<string> icons = new List<string>()
+          {
+               "!", "!", "N", "N", ",", ",", "k", "k",
+               "b", "b", "v", "v", "w", "w", "z", "z"
+          
+          };
+          public Form1()
+          {
+               InitializeComponent();
+               AssginIconsToSquare();
+
+          }
+
+          private void AssginIconsToSquare()
+          {
+               foreach (Control control in tableLayoutPanel1.Controls)
+               {
+                    Label iconLabel = control as Label;
+                    if (iconLabel != null)
+                    {
+                         int randomNumber = random.Next(icons.Count);
+                         iconLabel.Text = icons[randomNumber];
+
+                         icons.RemoveAt(randomNumber);
+                    }
+               }
+          }
+         
+     }
+}
